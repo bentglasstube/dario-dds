c5bd7fca60a1dc4a06c5c0db3b4dcdb7dc4a7135
Alan Berndt <alan@eatabrick.org>
2015-04-18 12:28:40 -0700

    Fix tooth sprite mappings

--- src/tooth.cc
+++ src/tooth.cc
@@ -9,16 +9,16 @@ void Tooth::get_source(SDL_Rect* rect) {
   unsigned int y = 0;
 
   switch (connections) {
-    case  0: x = 4, y = 4; break;
-    case  1: x = 4, y = 0; break;
-    case  2: x = 0, y = 4; break;
+    case  0: x = 3, y = 3; break;
+    case  1: x = 3, y = 0; break;
+    case  2: x = 0, y = 3; break;
     case  3: x = 0, y = 0; break;
-    case  4: x = 2, y = 4; break;
+    case  4: x = 2, y = 3; break;
     case  5: x = 2, y = 0; break;
-    case  6: x = 1, y = 4; break;
+    case  6: x = 1, y = 3; break;
     case  7: x = 1, y = 0; break;
-    case  8: x = 4, y = 2; break;
-    case  9: x = 4, y = 1; break;
+    case  8: x = 3, y = 2; break;
+    case  9: x = 3, y = 1; break;
     case 10: x = 0, y = 2; break;
     case 11: x = 0, y = 1; break;
     case 12: x = 2, y = 2; break;
